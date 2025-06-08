a153167293bf60ecf07fe5059ab9db14e4f12b59
Borislav Gavrilov <136329010+borislavGavrilov@users.noreply.github.com>
2025-06-08 11:46:10 +0300

    Adding required on Schema definitions

--- WorkShop-MagicMovie/src/models/movie.js
+++ WorkShop-MagicMovie/src/models/movie.js
@@ -1,14 +1,55 @@
 import { Schema ,model} from "mongoose";
 
+let maxYear = new Date().getFullYear() + 5
+const urlRegex =  /^https?:\/\/.+/
+
 const moviesSchema = new Schema({
-   title: String ,
-   category: String ,
-   genre: String,
-   director: String,
-   year:Number,
-   imageUrl: String,
-   rating:Number,
-   description:String,
+   title:{
+    type:String ,
+    required:[true , 'This field is required']
+   },
+
+   category:{
+    type:String ,
+    required:[true , 'This field is required']
+   },
+
+   genre: {
+    type:String ,
+    required:[true , 'This field is required']
+   },
+
+   director:{
+    type:String ,
+    required:[true , 'This field is required']
+   },
+
+   year:{
+    type: Number ,
+    required:[true , 'This field is required'],
+    min: [1930 , 'Min year is 1930'],
+    max: [maxYear , 'Max year is from this year + 5 year afther that'],
+   },
+
+   imageUrl: {
+      
+    type: String ,
+    required:[true , 'This field is required'],
+    match : [urlRegex , 'This field must start only with HTTP/HTTPS link']
+   },
+
+   rating:{
+      type:Number,
+      required:[true , 'This field is required'],
+      min : [1 , 'Min raiting is 1'],
+      max : [10 , 'Max raiting is 10']
+   },
+
+   description:{
+      type:String , 
+      required: [true , 'This field is required']
+      
+   }
 })
 
 const Movie = model('Movie' , moviesSchema)
